7b09853e68849c1a6157d62cde5210c49bf97e96
Oluwatimilehin Abiodun <oluwatimilehinabiodun64@gmail.com>
2026-06-27 17:42:28 +0100

    feat(api): define attachment routes for tasks

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\AttachmentController;
 
 
 
@@ -45,10 +46,20 @@ Route::prefix('v1')->group(function () {
                 Route::put('/tasks/{task}', [TaskController::class, 'update']);
                 Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 
+                Route::prefix('/tasks/{task}')
+                    ->group(function()
+                    {
+                        Route::get('/attachments', [AttachmentController::class, 'index']);
+                        Route::post('/attachments', [AttachmentController::class, 'store']);
+                        Route::get('/attachments/{attachment}', [AttachmentController::class, 'show']);
+                        Route::get('/attachments/{attachment}/download', [AttachmentController::class, 'download']);
+                        Route::delete('/attachments/{attachment}', [AttachmentController::class, 'destroy']);
+                    });
+                
                 Route::post('/invitations', [InvitationController::class, 'store']);
                 Route::get('/invitations', [InvitationController::class, 'index']);
                 Route::delete('/invitations/{invitation}', [InvitationController::class,'destroy']);
-                Route::post('/invitations/{invitation}/resend', [InvitationController::class, 'resend']);
+                Route::post('/invitations/{invitation}/resend', [InvitationController::class, 'resend']);        
         });   
         
     });
@@ -61,41 +72,3 @@ Route::prefix('v1')->group(function () {
     Route::post('/invitations/{token}/reject', [InvitationController::class, 'reject'])
         ->name('invitations.reject');
 });
-
-
-
-// Route::prefix('v1')->group(function () {
-    
-//     // Auth: Restricted by 'auth' rate limiter (5 req/min)
-//     Route::middleware('throttle:auth')->group(function () {
-//         Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
-//         Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-//     });
-
-//     // Protected routes: Requires valid Sanctum token
-//     Route::middleware('auth:sanctum')->group(function () {
-        
-//         Route::get('/user', function (Request $request) {
-//             return new UserResource($request->user());
-//         });
-
-//         Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-
-//         // Organisation-specific nested resources
-//         Route::middleware('org_context')->group(function () {
-            
-//             // 1. Automatically builds all 5 CRUD routes for projects:
-//             // organisations/{organisation}/projects
-//             // organisations/{organisation}/projects/{project}
-//             // ->scoped() ensures a 404 is thrown if a project doesn't belong to the organisation
-//             Route::apiResource('organisations.projects', ProjectController::class)->scoped([
-//                 'organisation' => 'id',
-//                 'project' => 'id',
-//             ]);
-
-//             // 2. Extra shallow/nested routes like tasks can be bound cleanly:
-//             Route::get('organisations/{organisation}/tasks', [TaskController::class, 'index'])->name('organisations.tasks.index');
-            
-//         });   
-//     });
-// });
